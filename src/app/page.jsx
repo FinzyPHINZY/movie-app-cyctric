0f0f4f282f26d6ffa0d62aa763cf1791ed54b5a7
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-12-18 09:58:34 +0100

    chore: set up Next.js project with Tailwind CSS and PNPM

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col h-[100vh] bg-[#093545] justify-center items-center">
+      <p>hello</p>
+      <p>world</p>
+    </div>
+  );
+}
